3a9ff6e2ea3a3917e9096075776d1909fd176781
Zudhakar <95558938+Zudhakar@users.noreply.github.com>
2022-02-19 13:54:52 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIA5Y5J6C7FCG4WQMUN"
+}
+variable "aws_secret_key" {
+default = "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"
+ }
+variable "region" {
+    default = "us-east-1"
+}
